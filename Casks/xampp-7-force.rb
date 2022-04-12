477d3e2c1bd2693e1e2bd2340b80fa6b39a9052d
Ondřej Hotový <ondrej.hotovy@outlook.com>
2022-04-12 15:00:54 +0200

    Initial Commit

---
+++ Casks/xampp-7-force.rb
@@ -0,0 +1,30 @@
+cask "xampp-7-force" do
+  version "7.4.28-1"
+  sha256 "448af59373a51f02218ea38bbafdf77bb25a95f3a67700bd3daa045865793bdf"
+
+  url "https://downloadsapachefriends.global.ssl.fastly.net/xampp-files/#{version.split("-").first}/xampp-osx-#{version}-installer.dmg",
+      verified: "downloadsapachefriends.global.ssl.fastly.net/xampp-files/"
+  name "XAMPP"
+  desc "Apache distribution containing MySQL, PHP 7, and Perl"
+  homepage "https://www.apachefriends.org/index.html"
+
+  livecheck do
+    url "https://www.apachefriends.org/download.html"
+    regex(%r{href=.*?/xampp[._-]osx[._-]v?(7(?:\.\d+)*-\d+)[._-]installer\.dmg}i)
+  end
+
+  installer script: {
+    executable: "xampp-osx-#{version}-installer.app/Contents/MacOS/osx-x86_64",
+    args:       ["--mode", "unattended"],
+    sudo:       true,
+  }
+
+  uninstall quit:   "com.bitnami.manager",
+            script: {
+              executable: "/Applications/XAMPP/uninstall.app/Contents/MacOS/osx-x86_64",
+              args:       ["--mode", "unattended"],
+              sudo:       true,
+            },
+            delete: "/Applications/XAMPP/"
+end
+
